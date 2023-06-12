160c81b7adf326427709ba4adfde877424469673
Phat Luong <luongtanphat25@gmail.com>
2023-06-12 13:10:46 -0400

    add html to urls_index file

--- express_server.js
+++ express_server.js
@@ -22,14 +22,20 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+  const templateVars = { greetings: 'Hello World!' };
+  res.render('hello_world', templateVars);
 });
 
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get('/set', (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+
+// app.get('/fetch', (req, res) => {
+//   res.send(`a = ${a}`);
+// });
