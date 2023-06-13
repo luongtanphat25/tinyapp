23263b4fa5b96cdfde81cdb4753f3f9167936c57
Phat Luong <luongtanphat25@gmail.com>
2023-06-13 16:43:10 -0400

    displayed username after login

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
@@ -28,7 +30,10 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render('urls_index', templateVars);
 });
 
